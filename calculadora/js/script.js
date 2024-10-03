aec3326287bce7381a84d6e7c63ee8388d30cfa0
profLedson <ledson.vanini@escola.pr.gov.br>
2024-10-03 15:22:33 +0000

    Iniciado o código Javascript

---
+++ calculadora/js/script.js
@@ -0,0 +1,15 @@
+// FAZ REFERÊNCIA AOS ELEMENTOS DOM
+const numero1 = document.querySelector('#numero1')
+const numero2 = document.querySelector('#numero2')
+const btnCalcular = document.querySelector('#btn-calcular')
+const resultado = document.querySelector('.resultado')
+const adicao = document.querySelector('.adicao')
+const subtracao = document.querySelector('.subtracao')
+const multiplicacao = document.querySelector('.multiplicacao')
+const divisao = document.querySelector('.divisao')
+
+//alert(numero1)
+// CRIA A FUNÇÃO CALCULAR
+
+
+// ADICIONA ESCUTAR DE EVENTOS
